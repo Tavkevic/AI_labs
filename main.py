94f73c59891acd3ceb719017a6525973c4712a1c
artem <tavkevicwork@gmail.com>
2025-03-27 19:28:38 +0300

    Малая экспертная система. Добавил веса для вопросов.

--- main.py
+++ main.py
@@ -2,23 +2,23 @@ def is_butterfly():
     print("Ответьте на вопросы (Да/Нет):")
 
     questions = [
-        "У объекта есть крылья?",
-        "Крылья покрыты чешуйками?",
-        "Питается нектаром?",
-        "Проходит стадию гусеницы?",
-        "Активен днем?"
+        ["У объекта есть крылья?", 1],
+        ["Крылья покрыты чешуйками?", 0.8],
+        ["Питается нектаром?", 0.8],
+        ["Проходит стадию гусеницы?", 1],
+        ["Активен днем?", 0.4]
     ]
 
     answers = []
     for q in questions:
-        ans = input(q + " ").strip().lower()
-        answers.append(ans == 'да')
+        ans = input(q[0] + " ").strip().lower()
+        if ans == 'да':
+            answers.append(1 * q[1])
 
     true_count = sum(answers)
-
-    if true_count == 5:
+    if true_count == 4:
         print("Да, это бабочка!")
-    elif true_count >= 3:
+    elif true_count >= 2.8:
         print("Возможно, это бабочка.")
     else:
         print("Скорее всего, это не бабочка.")
